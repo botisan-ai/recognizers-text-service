d9971dacf66b99c60556585c6978c35b4818bc6a
Simon Liang <simon@x-tech.io>
2022-11-01 18:23:50 +0800

    Update RecognizeInput.cs

--- Recognizer/Models/RecognizeInput.cs
+++ Recognizer/Models/RecognizeInput.cs
@@ -3,21 +3,21 @@ namespace Recognizer.Models
     public class RecognizeInput
     {
         public string Text { get; set; }
-        public string Culture { get; set; }
+        public string Culture? { get; set; }
     }
 
     public class RecognizeNumberWithUnitInput : RecognizeInput
     {
-        public List<string> Unit { get; set; }
+        public List<string> Unit? { get; set; }
     }
 
     public class RecognizeRangeWithUnitsInput : RecognizeNumberWithUnitInput
     {
-        public bool ShowNumbers { get; set; }
+        public bool ShowNumbers? { get; set; }
     }    
     
     public class RecognizeCombinedInput: RecognizeRangeWithUnitsInput {
-        public List<string> Entities { get; set; }
-        public bool MergeResults { get; set; }
+        public List<string> Entities? { get; set; }
+        public bool MergeResults? { get; set; }
     }
 }
